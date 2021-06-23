f86a44c7fa1a168e09fb1ff8fdd4cda4031c1bab
PamelaSohWY <pamelasohwy@gmail.com>
2021-06-23 05:47:38 +0000

    added hbs and actors view

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 
 const express = require('express');
 const hbs = require('hbs');
-const wax = require ('wax-on');
+const wax = require('wax-on');
 const mysql = require('mysql2/promise'); //yarn add mysql2 //do in command promppt
 
 //create express app
-let app= express();
+let app = express();
 app.set('view engine', 'hbs');
 
 //all css, image file and js file in public folder 
@@ -18,7 +18,9 @@ wax.on(hbs.handlebars);
 wax.setLayoutPath('./views/layouts');
 
 // set up forms handling
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({
+    extended: false
+}));
 
 // routes
 //this is just a test route initially
@@ -27,24 +29,29 @@ app.use(express.urlencoded({extended:false}));
 // }
 // )
 
-async function main(){
+async function main() {
 
     const connection = await mysql.createConnection({
-        host:'localhost',
-        user:'root',
+        host: 'localhost',
+        user: 'root',
         'database': 'sakila'
     })
 
-    app.get('/', (req,res)=>{
-        res.send("Hello world");
+    app.get('/', async (req, res) => {
+        // res.send("Hello world");
+        let [actors] = await connection.execute("select * from actor"); //using sql query // just to show infor in the browser // but to be readable need to create views hbs/ layout
+        res.render('actors', {
+            'actors': actors
+        }) 
     })
-
+    //go create base.hbs in layouts 
+    //create actors in views 
 }
 
 main();
 
 
 // start server
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("Server has started")
 })
